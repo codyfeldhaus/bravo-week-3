ee4448351e98dd7c5d922fefd9cf5229ac420b4f
Cody Feldhaus <cody.feldhaus@gmail.com>
2023-09-21 19:26:17 -0700

    Update destructuring.js

--- destructuring.js
+++ destructuring.js
@@ -4,7 +4,7 @@
 //by avoiding needing to access values with indices or keys every single time
 
 //array destructuring
-const fruits = ["apple", "banana", "cherry", "durian", "elderberry"]
+// const fruits = ["apple", "banana", "cherry", "durian", "elderberry"]
 
 //'old' way
 // const firstFruit = fruits[0];
@@ -14,18 +14,18 @@ const fruits = ["apple", "banana", "cherry", "durian", "elderberry"]
 // const fifthFruit = fruits[4];
 
 //with destructuring
-const [firstFruit, secondFruit, thirdFruit, fourthFruit, fifthFruit] = fruits;
+// const [firstFruit, secondFruit, thirdFruit, fourthFruit, fifthFruit] = fruits;
 
 // console.log(firstFruit);
 // console.log(fourthFruit);
 
 
 //object destructuring
-const person = {
-  firstName: "John",
-  lastName: "Doe",
-  age: 30
-};
+// const person = {
+//   firstName: "John",
+//   lastName: "Doe",
+//   age: 30
+// };
 
 //'old' way
 // const first = person.firstName;
@@ -35,7 +35,7 @@ const person = {
 //with destructuring
 //{originalProperty: newVariable}
 // const { firstName: first, lastName: last, age: age } = person;
-const {firstName, lastName, age} = person;
+// const {firstName, lastName, age} = person;
 // console.log(first, last, age);
 
 // const person2 = {
@@ -51,14 +51,14 @@ const {firstName, lastName, age} = person;
 
 
 //simulating API data
-const user = {
-  id: 1,
-  name: "John Doe",
-  address: {
-    street: '123 Main St',
-    city: 'Anytown'
-  }
-};
+// const user = {
+//   id: 1,
+//   name: "John Doe",
+//   address: {
+//     street: '123 Main St',
+//     city: 'Anytown'
+//   }
+// };
 
 //without destructuring:
 // console.log(user.name);
@@ -80,11 +80,49 @@ const user = {
 
 
 //if you're fine using the same name as the original property
-const {
-  name,
-  address: {street, city}
-} = user;
+// const {
+//   name,
+//   address: {street, city}
+// } = user;
 
-console.log(name); //instead of user.name
-console.log(street); //instead of user.address.street
-console.log(city); //instead of user.address.city
+// console.log(name); //instead of user.name
+// console.log(street); //instead of user.address.street
+// console.log(city); //instead of user.address.city
+
+
+//Ex 1
+const rgb = [255, 200, 0]
+
+//use destructuring to log out statement that looks like:
+//Red: 255, Green: 200, Blue: 0
+
+const [r, g, b] = rgb //[r, g, b] = [255, 200, 0]
+console.log(`Red: ${r}, Green: ${g}, Blue: ${b}`);
+
+//Ex 2
+const student = {
+  name: 'Jane Doe',
+  age: 25,
+  scores: {
+    mathematics: 85,
+    english: 90
+  }
+};
+//use destructuring to log out statement that looks like:
+// Name: Jane Doe, Age: 25, Mathematics Score: 85, English Score: 90
+//long way
+console.log(`Name: ${student.name}, Age: ${student.age}, 
+            Mathematics Score: ${student.scores.mathematics}, 
+            English Score: ${student.scores.english}`)
+
+//destructure the object
+const { 
+  name, 
+  age, 
+  scores: { 
+    mathematics, 
+    english 
+  } 
+} = student;
+
+console.log(`Name: ${name}, Age: ${age}, Mathematics Score: ${mathematics}, English Score: ${english}`);
